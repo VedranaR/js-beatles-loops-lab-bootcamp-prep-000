e2d54e0a0e5a30f0d35f31d8b7541801c9c76408
Vedrana <vedrana_ravlic16@yahoo.com>
2018-01-28 00:26:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for(let i=0; i < (musicians.length); i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return arr;
+}
+
+
+
+function johnLennonFacts (facts){
+  var arrayy = [];
+  var i= 0;
+  i++;
+  while (i<facts.length){
+   arrayy.push(`"${facts[i]} + !!!"`);
+  }
+  return arrayy;
+}
